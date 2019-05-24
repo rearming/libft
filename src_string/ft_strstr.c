ecf343759dd0baa71044e097d5b028a0fd1c5220
Snapper Leonardo <sleonard@oa-l4.21-school.ru>
2019-05-24 15:48:03 +0300

    libft now contains ft_printf

---
+++ src_string/ft_strstr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sleonard <sleonard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/03 18:10:50 by sleonard          #+#    #+#             */
+/*   Updated: 2019/05/13 11:19:47 by sleonard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	size_t	i;
+
+	i = 0;
+	if (needle[0] == '\0')
+		return ((char*)haystack);
+	while (haystack[i] != '\0')
+	{
+		if (haystack[i] == needle[0])
+		{
+			if (ft_strncmp(&haystack[i], &needle[0], ft_strlen(needle)) == 0)
+				return ((char*)&haystack[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
